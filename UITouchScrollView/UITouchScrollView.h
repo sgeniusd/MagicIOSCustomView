966d43bb83019e018f065c4f8794da1d57ac6774
magic <gaomeina@m.local>
2014-11-17 10:16:56 +0800

    自定义ScrollView，可以接受Touchbegan事件

---
+++ UITouchScrollView/UITouchScrollView.h
@@ -0,0 +1,13 @@
+//
+//  UITouchScrollView.h
+//  PrivateKitchen
+//
+//  Created by hw on 14-9-13.
+//  Copyright (c) 2014年 hw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITouchScrollView : UIScrollView
+
+@end
---
+++ UITouchScrollView/UITouchScrollView.m
@@ -0,0 +1,62 @@
+//
+//  UITouchScrollView.m
+//  PrivateKitchen
+//
+//  Created by hw on 14-9-13.
+//  Copyright (c) 2014年 hw. All rights reserved.
+//
+
+#import "UITouchScrollView.h"
+
+@implementation UITouchScrollView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    
+    // If not dragging, send event to next responder
+    if (!self.dragging){
+        [self.nextResponder touchesBegan: touches withEvent:event];
+    }
+    else{
+        [super touchesBegan: touches withEvent: event];
+    }
+}
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    
+    // If not dragging, send event to next responder
+    if (!self.dragging){
+        [self.nextResponder touchesMoved: touches withEvent:event];
+    }
+    else{
+        [super touchesMoved: touches withEvent: event];
+    }
+}
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    
+    // If not dragging, send event to next responder
+    if (!self.dragging){
+        [self.nextResponder touchesEnded: touches withEvent:event];
+    }
+    else{
+        [super touchesEnded: touches withEvent: event];
+    }
+}
+
+@end
